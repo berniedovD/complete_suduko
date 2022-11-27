5686641e182cdffcb2f6937616bb99db9e911f06
Dov <dov@medadoc.com>
2022-11-27 15:43:49 +0000

    start cleaning up the project

--- sudukofront/src/components/loadDefaultPuzzle.jsx
+++
@@ -1,6 +0,0 @@
-import { getInitialState } from "./sudukoHelperFunctions.js";
-export function loadDefaultPuzzle() {
-  let puzString = getInitialState();
-  let puzzList = [{ puzzleID: "p1", sudukoString: puzString }];
-  return puzzList;
-}
