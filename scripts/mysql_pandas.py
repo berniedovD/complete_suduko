ce8596850d49438b24c46aa907e943de9e53da9b
Dov Davidovics <dov@medadoc.com>
2022-11-27 23:00:33 -0800

    add pandas script to read mysql table

---
+++ scripts/mysql_pandas.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+
+hostname = r'52.90.144.5'
+
+dbport  = r'3306'
+dbname = 'appdb'
+dbuser = 'appdbuser'
+dbpwd = 'tortola1'
+
+dburl = URL.create('mysql',
+                              host=hostname,
+                              port=dbport,
+                              database=dbname,
+                              username=dbuser,
+                              password=dbpwd)
+db_engine = create_engine(dburl)
+
+dbconn = db_engine.connect()
+
+puzlist = pd.read_sql('select * from sudukoapi_puzzle', dbconn)
