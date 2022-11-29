11f4f4bedc0c47fe12e9bb51df0e7f34a6b572ec
Dov <dov@medadoc.com>
2022-11-29 16:43:33 +0000

    working except for call to backend adds /play and fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sleep 15 
+#sleep 10
 python manage.py collectstatic --no-input
 
-python manage.py migrate
-python manage.py createsuperuser --no-input --username=admin --email=dov@gyfu.com
+#python manage.py migrate
+#python manage.py createsuperuser --no-input --username=admin --email=dov@gyfu.com
 gunicorn -c gunicorn_django.py project3.wsgi
 
