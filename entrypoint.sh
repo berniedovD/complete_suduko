5c7ea87a524a737eb9af506fd3ef174712d70320
Dov <dov@medadoc.com>
2022-11-29 18:26:57 +0000

    go back to fresh new db create everything from scratch

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ sleep 15
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-#python manage.py createsuperuser --no-input --username=admin --email=dov@gyfu.com
+python manage.py createsuperuser --no-input --username=admin --email=dov@gyfu.com
 gunicorn -c gunicorn_django.py project3.wsgi
 
