eea232d778c3bcd4d9260f2273a225fee6796062
Dov <dov@medadoc.com>
2022-11-29 18:10:17 +0000

    tweak app entrypoint to wait and do migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#sleep 10
+sleep 15
 python manage.py collectstatic --no-input
 
-#python manage.py migrate
+python manage.py migrate
 #python manage.py createsuperuser --no-input --username=admin --email=dov@gyfu.com
 gunicorn -c gunicorn_django.py project3.wsgi
 
