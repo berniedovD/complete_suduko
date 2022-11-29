95c080b9fd000b4c66ab6ff4c70c888a765434fa
Dov <dov@medadoc.com>
2022-11-29 00:08:49 +0000

    nothing works

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sleep 15 
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 
 python manage.py migrate
 python manage.py createsuperuser --no-input --username=admin --email=dov@gyfu.com
