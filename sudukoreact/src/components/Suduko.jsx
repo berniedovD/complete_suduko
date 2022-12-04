74fcf88006762987e223b70eb217a963b1655a9a
Dov Davidovics <dov@medadoc.com>
2022-12-04 09:08:45 -0800

    start cleanup dead code

--- sudukoreact/src/components/Suduko.jsx
+++ sudukoreact/src/components/Suduko.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 
-import { checkFullPuzzle } from "./sudukoHelperFunctions";
 import sudukoSolveOneCell from "./sudukoSolveOneCell";
 import LoadPuzzle from "./loadPuzzle";
 
-import { buildJustPuzzle } from "./buildJustPuzzle";
 import { selectClickedCell } from "./selectClickedCell";
 // eslint-disable-next-line
 function handleAutoButton(setAutoMode) {
@@ -24,15 +22,10 @@ function autoSolve(setPuzzle, setDirection, puzzle, direction, autoMode) {
 }
 ///////////////////////////////////////////////////////////////////////
 function Suduko() {
+  console.log("Entering Suduko function");
   // eslint-disable-next-line
   function handleClickCell(row, col) {
     return selectClickedCell(fullState, row, col);
-  }
-  function handleAuto() {
-    console.log("clicked auto");
-    let [newPuzzle, newDirection] = sudukoSolveOneCell(puzzle, direction);
-    setPuzzle(newPuzzle);
-    setDirection(newDirection);
   }
   console.log("top of Suduko function - top");
 
@@ -53,44 +46,13 @@ function Suduko() {
   };
 
   ///////
-  if (puzzle.length !== 0) {
-    console.log("Puzzle length NOT 0");
-    console.log(`Puzzle length=${puzzle.length}`);
-    console.log(`Automode=${autoMode}`);
-    if (autoMode) {
-      setTimeout(handleAuto, 1);
-    }
-    let iv = checkFullPuzzle(puzzle);
-    fullState.iv = iv;
-    console.log(`iv=${iv}`);
-    let htmlToRender = buildJustPuzzle(fullState, handleClickCell);
-    let fullPage = (
-      <React.Fragment>
-        <button className="button-9" onClick={handleAuto}>
-          Single Step Auto Solve
-        </button>
-        <button
-          className="button-9"
-          onClick={() => {
-            handleAutoButton(setAutoMode);
-          }}
-        >
-          Run Auto Solve
-        </button>
-        {htmlToRender}
-        <h1>V3</h1>
-      </React.Fragment>
-    );
 
-    return fullPage;
-  } else {
-    console.log("No puzzle to render ");
-    return (
-      <React.Fragment>
-        <LoadPuzzle setPuzzle={fullState.setPuzzle}></LoadPuzzle>
-      </React.Fragment>
-    );
-  }
+  console.log("No puzzle to render ");
+  return (
+    <React.Fragment>
+      <LoadPuzzle setPuzzle={fullState.setPuzzle}></LoadPuzzle>
+    </React.Fragment>
+  );
 }
 
 export default Suduko;
