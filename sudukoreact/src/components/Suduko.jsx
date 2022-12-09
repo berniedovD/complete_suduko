b1d6f9c7411c2da9ff0f5ce5247a32b800e63156
Dov Davidovics <dov@medadoc.com>
2022-12-08 17:53:20 -0800

    add instructions change button sizes and colors

--- sudukoreact/src/components/Suduko.jsx
+++ sudukoreact/src/components/Suduko.jsx
@@ -78,22 +78,32 @@ function Suduko() {
     let htmlToRender = buildJustPuzzle(fullState, handleClickCell);
     let fullPage = (
       <React.Fragment>
-        <button className="button-9" onClick={handleAuto}>
-          Single Step Auto Solve
-        </button>
-        <button
-          className="button-9"
-          onClick={() => {
-            handleAutoButton(setAutoMode);
-          }}
-        >
-          Run Auto Solve
-        </button>
+        <div className="puzbuttons">
+          <h2>
+            To manually solve puzzle: click on cell or navigate to cells with
+            left and right buttons. Once cell is highlighted - type a number
+            from 1 -9 to fill cell - type 0 to empty cell.
+          </h2>
+          <div>
+            {" "}
+            <button className="button-9" onClick={handleAuto}>
+              Single Step Auto Solve
+            </button>
+          </div>
+          <div>
+            <button
+              className="button-9 bt-green"
+              onClick={() => {
+                handleAutoButton(setAutoMode);
+              }}
+            >
+              Run Auto Solve
+            </button>
+          </div>
+        </div>
         {htmlToRender}
-        <h1>V3</h1>
       </React.Fragment>
     );
-
     return fullPage;
   } else {
     console.log("No puzzle to render ");
